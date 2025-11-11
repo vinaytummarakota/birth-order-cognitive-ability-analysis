925f755860ab2f00b69ff11d9c1700b5e74ba38e
sst7 <sst7@rice.edu>
2025-11-10 23:09:30 -0500

    Finished data cleaning (I think)

--- 01_clean_data/01_helper_functions.R
+++ 01_clean_data/01_helper_functions.R
@@ -1,7 +1,14 @@
 library(tidyverse)
 library(NlsyLinks)
+library(docstring)
 
 clean_df <- function(df, old_cols, new_cols, dropped_cols) {
+  #' @description: Clean the input NLSY data 
+  #' 
+  #' @param df: The input NLSY data
+  #' @param old_cols: The column names in the original NLSY data obtained via the NLS Investigator
+  #' @param new_cols: The new column names that should be used in place of the old column names
+  #' @param dropped_cols: The columns that should be dropped because they won't be used in this analysis, but are automatically added by the NLS Investigator
   cleaned_df <- df %>%
     rename(
       !!!set_names(old_cols, new_cols)
@@ -91,14 +98,20 @@ clean_df <- function(df, old_cols, new_cols, dropped_cols) {
         NA
       )
     ) %>%
+    # because the NLSY79 did not report the exact age at which respondents completed the ASVAB, I approximated age using the respondents' birth month and the month they completed the 1981 interview (the interview which took place the year after the ASVAB was administered)
     mutate(
       estimated_age = estimate_age(
         month_of_birth, 
         year_of_birth, 
         month_of_1981_interview
       )
+    ) %>%
+    # I remove observations which do not have an ASVAB sampling weight (i.e. they did not complete the ASVAB)
+    filter(
+      !is.na(sampling_weight)
     )
   
+  # I use the NlsyLinks package to identify full biological siblings
   family_linkages_df <- Links79Pair %>%
     filter(
       R == 0.5, 
@@ -121,9 +134,7 @@ clean_df <- function(df, old_cols, new_cols, dropped_cols) {
     ) %>%
     distinct() 
   
-  # While the {sibling_id} is technically equal to the {family_id} (and thus, I could join by {sibling_id} = {family_id}), I don't do this because I want to distinguish between two non-full-sibling respondents who live in the same household vs two full-sibling respondents who live in the same household. 
-  
-  # In the former case, the {sibling_id} would be NA; in the latter case, it would not. Thus, I only join by {case_id} 
+  # While the {sibling_id} is technically equal to the {family_id} (and thus, I could join by {sibling_id} = {family_id}), I don't do this because I want to distinguish between two non-full-sibling respondents who live in the same household vs two full-sibling respondents who live in the same household. In the former case, the {sibling_id} would be NA; in the latter case, it would not.
   cleaned_df <- merge(cleaned_df, family_linkages_df, by = c("case_id"), all.x = TRUE)
   
   cleaned_df <- estimate_num_full_sibling_respondents(cleaned_df)
@@ -136,6 +147,11 @@ clean_df <- function(df, old_cols, new_cols, dropped_cols) {
 }
 
 estimate_age <- function(month_of_birth, year_of_birth, month_of_1981_interview) {
+  #' @description: Estimate the age of the respondent at the time of their 1981 interview
+  #' 
+  #' @param month_of_birth: An integer indicating the month of the year in which the respondent was born
+  #' @param year_of_birth: A two-digit integer indicating the year in which the respondent was born
+  #' @param month_of_1981_interview: An integer indicating the month in which the respondent completed the 1981 interview
   birth_month <- year_of_birth * 12 + month_of_birth
   
   # because the {year_of_birth} variable is stored as a two-digit integer, we use '81' when calculating the interview month instead of '1981'
@@ -145,6 +161,9 @@ estimate_age <- function(month_of_birth, year_of_birth, month_of_1981_interview)
 }
 
 estimate_num_full_sibling_respondents <- function(df) {
+  #' @description: Estimate the number of full-sibling NLSY respondents in each family 
+  #' 
+  #' @param df: The input NLSY data
   grouped_df <- df %>%
     group_by(sibling_id) %>%
     summarise(
@@ -163,6 +182,9 @@ estimate_num_full_sibling_respondents <- function(df) {
 }
 
 identify_dz_twins <- function(df) {
+  #' @description: Identify likely dizygotic twins in the NLSY data and randomly select one to be included in any birth order analyses
+  #' 
+  #' @param df: The input NLSY data
   age_df <- df %>%
     filter(
       !is.na(sibling_id)
@@ -200,7 +222,7 @@ identify_dz_twins <- function(df) {
       is_likely_dz_twin = as.logical(max(is_likely_dz_twin))
     )
     
-  # for each set of DZ twins, select the one with the lowest {birth_order}; if the DZ twins have the same birth order, select the one with the lowest {case_id}
+  # for each set of DZ twins, select the one with the lowest {birth_order} to be included in the birth order analysis; if the DZ twins have the same birth order, select the one with the lowest {case_id}
   selected_twin_df <- grouped_df %>%
     filter(
       is_likely_dz_twin == TRUE
@@ -239,6 +261,9 @@ identify_dz_twins <- function(df) {
 }
 
 identify_analytic_sample <- function(df) {
+  #' @description: Identify the respondents who will be included in the two-sample and three-sample birth order analyses, respectively. 
+  #' 
+  #' @param df: The input NLSY data
   grouped_df <- df %>%
     filter(
       num_full_sibling_respondents >= 2, 
@@ -255,17 +280,23 @@ identify_analytic_sample <- function(df) {
     summarise(
       has_first_born_child = max(birth_order == 0), 
       has_second_born_child = max(birth_order == 1), 
-      has_third_born_child = max(birth_order == 2)
+      has_third_born_child = max(birth_order == 2), 
+      # unfortunately, there are a couple families where multiple non-DZ twin siblings report having the same birth order (which should be impossible), so I need to use these columns to remove them
+      has_anomalous_birth_order_two_sibling_sample = (sum(birth_order %in% c(0, 1)) != 2), 
+      has_anomalous_birth_order_three_sibling_sample = (sum(birth_order %in% c(0, 1, 2)) != 3)
     ) %>%
+    # identify the set of families which have the first-born, second-born, and third-born children in the NLSY data
     mutate(
       is_two_sibling_sample_family = (
         has_first_born_child & 
-        has_second_born_child
+        has_second_born_child & 
+        !has_anomalous_birth_order_two_sibling_sample
       ), 
       is_three_sibling_sample_family = (
         has_first_born_child & 
         has_second_born_child & 
-        has_third_born_child
+        has_third_born_child & 
+        !has_anomalous_birth_order_three_sibling_sample
       )
     ) %>%
     select(
@@ -279,8 +310,8 @@ identify_analytic_sample <- function(df) {
       # If the respondent ... 
       # 1. Belongs to an eligible family
       # 2. Has an eligible birth order
-      # 3. Is either a non-DZ twin or a selected DZ twin
-      # ... then they are included in the analytic sample
+      # 3. Is a selected DZ twin or a non-DZ twin
+      # ... then they should be included in the analytic sample
       is_in_two_sibling_sample = 
         is_two_sibling_sample_family &  
         birth_order %in% c(0, 1) & 
@@ -298,6 +329,59 @@ identify_analytic_sample <- function(df) {
           TRUE
         )
       )
+  
+  # in rare cases, siblings' birth orders don't align with their actual ages; the next steps ensure that those siblings are not included in the analytic sample
+  two_sibling_sample_eligible_families <- analytic_sample_df %>%
+    filter(
+      is_in_two_sibling_sample == TRUE
+    ) %>%
+    select(
+      family_id, 
+      estimated_age, 
+      birth_order
+    ) %>%
+    pivot_wider(
+      names_from = birth_order, 
+      values_from = estimated_age, 
+      names_prefix = 'estimated_age_'
+    ) %>%
+    # require that the first-born child has a higher age than the second-born
+    filter(
+      estimated_age_0 > estimated_age_1 
+    ) %>%
+    pull(
+      family_id
+    )
+  
+  three_sibling_sample_eligible_families <- analytic_sample_df %>%
+    filter(
+      is_in_three_sibling_sample == TRUE
+    ) %>%
+    select(
+      family_id, 
+      estimated_age, 
+      birth_order
+    ) %>%
+    pivot_wider(
+      names_from = birth_order, 
+      values_from = estimated_age, 
+      names_prefix = 'estimated_age_'
+    ) %>%
+    # require that the first-born child has a higher age than the second-born and third-born, second-born child has a higher age than the third-born
+    filter(
+      estimated_age_0 > estimated_age_1, 
+      estimated_age_0 > estimated_age_2, 
+      estimated_age_1 > estimated_age_2
+    ) %>%
+    pull(
+      family_id
+    )
+  
+  analytic_sample_df <- analytic_sample_df %>%
+    mutate(
+      is_in_two_sibling_sample = is_in_two_sibling_sample & family_id %in% two_sibling_sample_eligible_families, 
+      is_in_three_sibling_sample = is_in_three_sibling_sample & family_id %in% three_sibling_sample_eligible_families
+    )
   
   return(analytic_sample_df)
 }
